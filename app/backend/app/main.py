fc169bd70a9dafb303409dcbd5dd975f806b8af2
Hariharan Ragunathan <gehariharan@gmail.com>
2025-04-26 20:02:37 -0700

    Fixing BE main.py CORS

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust for production
+    allow_origins=["https://shastha.online","http://localhost:3000"],  # Adjust for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
